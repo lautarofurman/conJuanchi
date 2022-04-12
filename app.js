d939be920b2fcf555d6a311f1648ae8d4b1fcf6c
lautarofurman <furmanlautaro@gmail.com>
2022-04-11 21:19:39 -0300

    modificacion random en app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("lauti que capo que sos")
-let nombre = "lauti"
+let nombre = "lauti"    
 console.log(nombre + "naciste para ser el megaprogramer")
 
 console.log("el capo sos vos juanchi");
